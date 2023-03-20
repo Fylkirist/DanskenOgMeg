372a9305783bd3668fe9069b83c74c3c907a48e1
LarsCirco <122366416+LarsCirco@users.noreply.github.com>
2023-03-20 07:24:27 +0100

    login ferdig, mangler bare controller

--- src/components.js
+++ src/components.js
@@ -5,6 +5,7 @@ function loginView(){
     let meny = document.createElement('div');
     let inputUser = document.createElement('input');
     let inputPass = document.createElement('input');
+    let loginEnter = document.createElement('button');
 
     container.className = "LoginRegContainer";
     login.id="loginButton";
@@ -12,14 +13,17 @@ function loginView(){
     meny.id="dropdownList";
     inputUser.id="userNameInput";
     inputPass.id="passwordInput";
+    loginEnter.id="LoginKnapp"
 
     login.textContent = "Login";
     register.textContent = "Register";
     inputUser.textContent = "Username";
     inputPass.textContent = "Password";
+    loginEnter.textContent = "confirm"
 
     meny.appendChild(inputUser);
     meny.appendChild(inputPass);
+    meny.appendChild(loginEnter)
     meny.style.display = "none";
 
     login.addEventListener('click', function() {
@@ -33,6 +37,16 @@ function loginView(){
             }
         }
     );
+   loginEnter.addEventListener('click', function(){
+        if (inputPass && inputUser != "")
+            {
+            const username = inputUser.value;
+            const password = inputPass.value;
+            checkLogin(bruker,password)
+            }
+   }
+   );
+    
     container.appendChild(register)
     container.appendChild(login);
     container.appendChild(meny);
