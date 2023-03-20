1bc7c8a680af36c54ae1e53810029cfbb0c9f6e2
Fylkirist <112450556+Fylkirist@users.noreply.github.com>
2023-03-20 10:35:58 +0100

    productDisplay added

--- src/components.js
+++ src/components.js
@@ -46,3 +46,91 @@ function loginView(){
 
     
 }*/
+
+function productDisplay(product){
+    let container = document.createElement("div")
+    container.className = "productDisplayContainer"
+
+    let productTitle = document.createElement("div")
+    productTitle.className ="productDisplayTitle"
+    productTitle.textContent = model.data.items[product].title
+    
+    let descriptionContainer = document.createElement("div")
+    descriptionContainer.className = "productDisplayDescriptionContainer"
+
+    let imageContainer = document.createElement("img")
+    imageContainer.className = "productDisplayImage"
+    imageContainer.src = model.data.items[product].images[0]
+
+    let descriptionTitle = document.createElement("h1")
+    descriptionTitle.className = "productDisplayDescriptionTitle"
+    descriptionTitle.textContent = "Beskrivelse"
+
+    let productDescription = document.createElement("p")
+    productDescription.className = "productDisplayDescription"
+    productDescription.textContent = model.data.items[product].description
+
+    let priceLabel = document.createElement("label")
+    priceLabel.className = "productDisplayPriceLabel"
+
+    let price = document.createElement("div")
+    price.className = "productDisplayPrice"
+    price.textContent = model.data.items[product].price
+
+    let buyButton = document.createElement("button")
+    buyButton.className = "productDisplayBuyButton"
+
+    let purchaseContainer = document.createElement("div")
+    purchaseContainer.className = "productDisplayPurchaseContainer"
+
+    let galleryContainer = document.createElement("div")
+    galleryContainer.className = "productDisplayImageGallery"
+
+    let galleryHeader = document.createElement("h2")
+    galleryHeader.textContent = "Bildegalleri"
+    galleryHeader.className = "productDisplayGalleryHeader"
+
+    descriptionContainer.appendChild(descriptionTitle)
+    descriptionContainer.appendChild(imageContainer)
+    descriptionContainer.appendChild(productDescription)
+
+    galleryContainer.appendChild(galleryHeader)
+
+    container.appendChild(productTitle)
+    container.appendChild(descriptionContainer)
+    container.appendChild(galleryContainer)
+    
+    for(let i = 1; i<model.data.items[product].images.length;i++){
+        let img = document.createElement("img")
+        img.className = "productDisplayGalleryElement"
+        img.src = model.data.items[product].images[i]
+        img.onclick = blowUpGalleryImg(this)
+        galleryContainer.appendChild(img)
+    }
+
+    purchaseContainer.appendChild(priceLabel)
+    purchaseContainer.appendChild(price)
+
+    if(model.data.items[product].auction){
+        priceLabel.textContent = "Nåværende bud: "
+        
+        buyButton.textContent = "Legg inn bud"
+        buyButton.onclick = raiseBid(product)
+        
+        let increaseBid = document.createElement("input")
+        increaseBid.placeholder = "Øk bud"
+        increaseBid.oninput = model.inputs.product.bidIncrease = increaseBid.value
+
+        purchaseContainer.appendChild(increaseBid)
+    }
+    else{
+        priceLabel.textContent = "Pris: "
+        
+        buyButton.textContent = "Legg til i handlekurv"
+        
+        buyButton.onclick = addToShoppingCart(product)
+    }
+    purchaseContainer.appendChild(buyButton)
+    
+    return container
+}
