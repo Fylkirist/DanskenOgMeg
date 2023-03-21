b479f4eaa9511aa3946f9384325e6e8cfe3494d8
abir314 <121821310+abir314@users.noreply.github.com>
2023-03-21 07:48:05 +0100

    test

---
+++ src/categoryFilterController.js
@@ -0,0 +1,74 @@
+function showHideCategories(){
+
+    (model.inputs.category.wholeCategoryShow) ? model.inputs.category.wholeCategoryShow = false :
+                                               model.inputs.category.wholeCategoryShow = true;
+
+    updateView();
+}
+function selectMainCategory(mainCategoryIndex, trueOrFalse){
+    model.app.currentView = 'filteredPage';
+    if(trueOrFalse){
+        for (let i = 0; i < model.inputs.category.selectedMainCategories.length; i ++){
+            for(let j = 0; j < model.inputs.category.selectedMainCategories[i].selectSubCategories.length; j++){
+                model.inputs.category.selectedMainCategories[i].selectSubCategories[j].title = '';
+                model.inputs.category.selectedMainCategories[i].selectSubCategories[j].checked = false;
+            }
+        } 
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].title = model.data.itemsCategory[mainCategoryIndex].title;    
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].checked = true;
+        filteredCategories();
+    }
+    else if(!trueOrFalse){
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].title = '';    
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].checked = false;
+        for(let i = 0; i <model.inputs.category.selectedMainCategories[mainCategoryIndex].selectSubCategories.length; i++ ){
+            model.inputs.category.selectedMainCategories[mainCategoryIndex].selectSubCategories[i].checked = false;
+            model.inputs.category.selectedMainCategories[mainCategoryIndex].selectSubCategories[i].title = '';
+        }
+        filteredCategories();
+    }
+    
+
+    updateView();
+
+}
+function showHideSubCategories(mainCategoryIndex){
+    
+    (model.inputs.category.selectedMainCategories[mainCategoryIndex].showSubCategories) ? 
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].showSubCategories = false :
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].showSubCategories = true;
+
+    updateView();
+}
+function selectSubCategory(mainCategoryIndex, subCategoryIndex, trueOrFalse){
+    model.app.currentView = 'filteredPage';
+    if(trueOrFalse){
+        for(let i = 0; i < model.inputs.category.selectedMainCategories.length; i++){
+            if(i !== mainCategoryIndex){
+                model.inputs.category.selectedMainCategories[i].title = '';
+                model.inputs.category.selectedMainCategories[i].checked = false;
+                for(let j = 0; j < model.inputs.category.selectedMainCategories[i].selectSubCategories.length; j++){
+                    model.inputs.category.selectedMainCategories[i].selectSubCategories[j].title = '';
+                    model.inputs.category.selectedMainCategories[i].selectSubCategories[j].checked = false;
+                }
+            }   
+
+        }
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].title = model.data.itemsCategory[mainCategoryIndex].title;
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].checked = true;
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].selectSubCategories[subCategoryIndex].title = model.data.itemsCategory[mainCategoryIndex].subCategory[subCategoryIndex].title;    
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].selectSubCategories[subCategoryIndex].checked = true;
+        filteredCategories();
+    }
+    else if(!trueOrFalse){
+        // model.inputs.category.selectedMainCategories[mainCategoryIndex].title = '';
+        // model.inputs.category.selectedMainCategories[mainCategoryIndex].checked = false;
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].selectSubCategories[subCategoryIndex].title = '';    
+        model.inputs.category.selectedMainCategories[mainCategoryIndex].selectSubCategories[subCategoryIndex].checked = false;
+        filteredCategories();
+    }
+    
+
+    updateView();
+
+}
---
+++ src/filteredPageView.js
@@ -0,0 +1,74 @@
+function filteredPageUpdateView(){
+    let container = document.createElement('div');
+    container.id= 'filteredPageViewContainer';
+    container.appendChild(showCategory());
+
+    let products = document.createElement('div');
+    products.id = 'filteredProducts';
+
+    let categoryHeader = document.createElement('h4');
+    categoryHeader.textContent = 'Products in Selected Categories';
+    categoryHeader.id = 'categoryHeader';
+    products.appendChild(categoryHeader);
+
+    if(model.inputs.filteredProductPage.selectedMainCategories.length === 0 ) {
+        let showText = document.createElement('p');
+        showText.textContent = 'No categories have been chosen.';
+        products.appendChild(showText);
+    }
+    else if(model.inputs.filteredProductPage.selectedSubCategories.length > 0 ) {
+   
+            for(let i = 0; i < model.data.items.length; i++){
+                let productContainer = document.createElement('div');
+                productContainer.className = 'productContainer';
+
+                for(let j=0; j<model.data.items[i].category.length; j++){
+                    let categoryName = model.data.items[i].category[j];
+                    if (model.inputs.filteredProductPage.selectedSubCategories.includes(categoryName)){
+                        let productTitle = document.createElement('p');
+                        productTitle.textContent = model.data.items[i].title;
+                        productContainer.appendChild(productTitle);
+
+                        let productImage = document.createElement('img');
+                        productImage.src = model.data.items[i].images[0];
+                        productContainer.appendChild(productImage);
+                        products.appendChild(productContainer);
+                        break;
+                    }
+
+                }
+
+            }
+        }
+    else if (model.inputs.filteredProductPage.selectedMainCategories.length > 0){
+
+            for(let i = 0; i < model.data.items.length; i++){
+                let productContainer = document.createElement('div');
+                productContainer.className = 'productContainer';
+
+                for(let j=0; j<model.data.items[i].category.length; j++){
+                    let categoryName = model.data.items[i].category[j];
+
+                    if (model.inputs.filteredProductPage.selectedMainCategories.includes(categoryName)){
+                        let productTitle = document.createElement('p');
+                        productTitle.textContent = model.data.items[i].title;
+                        productContainer.appendChild(productTitle);
+
+                        let productImage = document.createElement('img');
+                        productImage.src = model.data.items[i].images[0];
+                        productContainer.appendChild(productImage);
+                        products.appendChild(productContainer);
+                        break;
+                    }
+
+                }
+
+            }
+        }
+    
+    
+    container.appendChild(products);
+
+
+    return container;
+}
