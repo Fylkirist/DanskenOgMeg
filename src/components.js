ea591562c378ea02abf8b2ec76ad2dd86a4f9c85
LarsCirco <122366416+LarsCirco@users.noreply.github.com>
2023-03-22 11:42:10 +0100

    Register med template litterals

--- src/components.js
+++ src/components.js
@@ -12,145 +12,37 @@ function updateView(){
 }
 
 
-function registerFormView(){
-let container = document.createElement("div");
+function registerFormView() {
 
-let contName = document.createElement('div')
-let inputFirstName = document.createElement('input');
-let inputLastName = document.createElement('input');
-inputFirstName.oninput = function (){
-    model.inputs.register.firstName = inputFirstName.value;
-};
-inputLastName.oninput = function(){
-    model.inputs.register.lastName = inputLastName.value;
-}
-
-
-let contPhoneMail = document.createElement('div')
-let inputMail = document.createElement('input');
-let inputPhone = document.createElement('input');
-inputMail.oninput = function(){
-    model.inputs.register.email = inputMail.value;
-}
-inputPhone.oninput = function(){
-    model.inputs.register.mobile = inputPhone.value;
-}
-
-
-
-let contUserPass = document.createElement('div');
-let inputUsername = document.createElement('input');
-let inputPassword = document.createElement('input');
-let inputRepeatPassword = document.createElement('input');
-inputUsername.oninput = function(){
-    model.inputs.register.userName = inputUsername.value;
-}
-inputPassword.oninput = function(){
-    model.inputs.register.password = inputPassword.value;
-}
-inputRepeatPassword.oninput = function(){
-model.inputs.register.repeatPassword = inputRepeatPassword.value}
-
-
-let inputAdress = document.createElement('input');
-let inputZip = document.createElement('input');
-let inputCity = document.createElement('input');
-let contAdress = document.createElement('div')
-
-inputAdress.oninput = function (){
-    model.inputs.register.address = inputAdress.value;
-}
-inputZip.oninput = function (){
-    model.inputs.register.zip = inputZip.value;
-}
-inputCity.oninput = function (){
-    model.inputs.register.city = inputCity.value;
-}
-
-
-
-
-let inputCard = document.createElement('input');
-let inputEndDate = document.createElement('input');
-let inputCve = document.createElement('input');
-let contCardInfo = document.createElement('div')
-inputCard.oninput = function (){
-    model.inputs.register.cardNumber = inputCard.value;
-}
-inputEndDate.oninput = function (){
-    model.inputs.register.fromDate = inputEndDate.value;
-}
-
-let Register = document.createElement('button')
-let Melding = document.createElement('div')
-Register.textContent = "Register";
-    Register.onclick = function () 
-        {
-            if(model.inputs.register.password !== model.inputs.register.repeatPassword)
-                {
-                    Melding.textContent = "Passordene matcher ikke"
-                    updateView()
-                    return;
-                }
-        RegisterPushUser();
-        }
-
-inputFirstName.placeholder = "First Name";
-inputLastName.placeholder= "Last Name"
-inputMail.placeholder="Email"
-inputPhone.placeholder="Telefon Nr:"
-inputUsername.placeholder="Brukernavn: "
-inputPassword.placeholder="Passord: "
-inputRepeatPassword.placeholder="Gjenta Passord: "
-inputAdress.placeholder = "Adresse: "
-inputZip.placeholder = "Post nummer: "
-inputCity.placeholder = "By: "
-inputCard.placeholder = "Card info"
-inputCve.placeholder = "CVE"
-inputEndDate.type = "date";
-
-
-
-contName.appendChild(inputFirstName);
-contName.appendChild(inputLastName);
-
-contPhoneMail.appendChild(inputMail)
-contPhoneMail.appendChild(inputPhone)
-
-contUserPass.appendChild(inputUsername)
-contUserPass.appendChild(inputPassword)
-contUserPass.appendChild(inputRepeatPassword)
-
-contAdress.appendChild(inputAdress)
-contAdress.appendChild(inputZip)
-contAdress.appendChild(inputCity)
-
-contCardInfo.appendChild(inputCard)
-contCardInfo.appendChild(inputEndDate)
-contCardInfo.appendChild(inputCve)
-
-contCardInfo.className="form-group"
-container.className="form-container"
-contAdress.className="form-group";
-contUserPass.className="form-group"
-contPhoneMail.className="form-group"
-contName.className="form-group"
-
-
-
-
-container.appendChild(contName)
-container.appendChild(contPhoneMail)
-container.appendChild(contUserPass)
-container.appendChild(contAdress)
-container.appendChild(contCardInfo)
-container.appendChild(Register)
-
-
-return container;
-
-
-
-
-        
-}
+    let container = /*html */`
+      <div class="form-container">
+        <div class="form-group">
+          <input type="text" placeholder="First Name" oninput="model.inputs.register.firstName = this.value;">
+          <input type="text" placeholder="Last Name" oninput="model.inputs.register.lastName = this.value;">
+        </div>
+        <div class="form-group">
+          <input type="email" placeholder="Email" oninput="model.inputs.register.email = this.value;">
+          <input type="tel" placeholder="Telefon Nr:" oninput="model.inputs.register.mobile = this.value;">
+        </div>
+        <div class="form-group">
+          <input type="text" placeholder="Brukernavn:" oninput="model.inputs.register.userName = this.value;">
+          <input type="password" placeholder="Passord:" oninput="model.inputs.register.password = this.value;">
+          <input type="password" placeholder="Gjenta Passord:" oninput="model.inputs.register.repeatPassword = this.value;">
+        </div>
+        <div class="form-group">
+          <input type="text" placeholder="Adresse:" oninput="model.inputs.register.address = this.value;">
+          <input type="text" placeholder="Post nummer:" oninput="model.inputs.register.zip = this.value;">
+          <input type="text" placeholder="By:" oninput="model.inputs.register.city = this.value;">
+        </div>
+        <div class="form-group">
+          <input type="text" placeholder="Card info" oninput="model.inputs.register.cardNumber = this.value;">
+          <input type="date" placeholder="Fra dato" oninput="model.inputs.register.fromDate = this.value;">
+          <input type="text" placeholder="CVE" oninput="model.inputs.register.cve = this.value;">
+        </div>
+        <button onclick="registerUser()">Register</button>
+        <div id="melding"></div>
+      </div>
+    `;
+    document.getElementById('app').innerHTML = container;
+    return container;
+  }
