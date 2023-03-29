a1fd0ee01e390e9bbfd9454f075209bdeeafb99d
Fylkirist <112450556+Fylkirist@users.noreply.github.com>
2023-03-29 09:46:48 +0200

    final cleanup before merge

--- src/controller.js
+++ src/controller.js
@@ -76,3 +76,412 @@ function increaseBid(itemsId, usersId, usersMaximumBid, newBud){
         updateView();
 }
 
+function registerUser() {
+    let inputs = model.inputs.register
+    if (inputs.firstName.length > 0 && // alle felter må være fylt ut før koden starter
+        inputs.lastName.length > 0 &&
+        inputs.email.length > 0 &&
+        inputs.mobile.length > 0 &&
+        inputs.userName.length > 0 &&
+        inputs.password.length > 0 &&
+        inputs.repeatPassword.length > 0 &&
+        inputs.address.length > 0 &&
+        inputs.zip.length > 0){    
+        let brukernavnTatt = false;
+        for (let userID in model.data.users){
+            if(model.data.users[userID].username === model.inputs.register.userName){
+                brukernavnTatt = true;
+                break
+            }
+        }
+        if(brukernavnTatt){    
+            model.inputs.register.meldingRegister = 'Brukernavn eksisterer allerede'
+            updateView()
+            return
+        }
+        if(model.inputs.register.cardNumber.length>0 && !isValid(model.inputs.register.cardNumber)){
+            model.inputs.register.meldingRegister = 'Ikke gyldig bankkort'
+            updateView()
+            return
+        }
+        if (model.inputs.register.password === model.inputs.register.repeatPassword){ 
+            const newUser = { 
+                username:model.inputs.register.userName,                        
+                password:model.inputs.register.password,  
+                permissions: 'user',
+                firstName:model.inputs.register.firstName,
+                surname:model.inputs.register.surname,
+                address:model.inputs.register.address,
+                email:model.inputs.register.email,
+                mobile:model.inputs.register.mobile,
+                shoppingCart:[],
+                auctionList:[],
+                paymentInformation:[
+                    {
+                        cardNumber: model.inputs.register.cardNumber,
+                        expirationDate:model.inputs.register.fromDate,
+                        cardHolderFirstName:model.inputs.register.firstName,
+                        cardHolderLastName:model.inputs.register.surname,
+                        address:model.inputs.register.address,
+                        zip:model.inputs.register.zip,
+                        houseNumber:"",                    
+                    }
+                ],
+                messages:[],
+            }
+            let brukerTelling = Object.keys(model.data.users).length +1
+            let newUserID = brukerTelling.toString()
+            while(newUserID.length<7){
+                newUserID = "0" + newUserID
+            }
+            model.data.users[newUserID] = newUser
+            model.inputs.register.registerUser = "Velkommen til Dansken & Meg antikkmarked"
+            model.app.currentView = "frontPage"
+        }
+        else if (model.inputs.register.password !== model.inputs.register.repeatPassword){
+            model.app.input.register.meldingRegister = 'Passordene du har skrevet stemmer ikke overens'
+        }
+    }
+    else {
+        model.inputs.register.meldingRegister = "Alle feltene må være fylt ut"
+    }
+    updateView()
+}
+
+function isValid(input) {
+    let cardNumber = input.replace(/\s/g,'')
+    if (/[^0-9-\s]+/.test(cardNumber)) return false
+    let sum = 0
+    let double = false
+    for (let i = cardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(cardNumber.charAt(i))
+      if (double) {
+        digit *= 2
+        if (digit > 9) digit -= 9
+      }
+      sum += digit
+      double = !double
+    }
+    return (sum % 10) == 0
+}
+
+function goBackToFrontPage(){
+    Object.values(model.inputs.register).forEach((val)=>val = "")
+    model.app.currentView = "frontPage"
+    updateView()
+}
+
+function goToRegisterPage(){
+    Object.values(model.inputs.login).forEach((val)=>val = "")
+    model.app.currentView = "registerPage"
+    updateView()
+}
+
+function doSearch() {
+    model.app.currentView = "filteredPage";
+    updateView();
+}
+
+function changeFrontPageBotProduct(direction){
+    if(model.app.frontPageCurrentShowing.bottom>0 && model.app.frontPageCurrentShowing.bottom < model.data.frontPageBottom.length-1){
+        model.app.frontPageCurrentShowing.bottom += direction
+    }
+    updateView()
+}
+
+function changeFrontPageTopProduct(direction){
+    if(model.app.frontPageCurrentShowing.top>0 && model.app.frontPageCurrentShowing.top < model.data.frontPageTop.length-1){
+        model.app.frontPageCurrentShowing.top += direction
+    }
+    updateView()
+}
+
+function loginDropDown(){
+    if(model.inputs.login.dropdown) {
+        model.inputs.login.dropdown = false;
+    }
+    else {
+        model.inputs.login.dropdown = true;
+    }
+    updateView();
+}
+
+function checkUserIdPassword(){
+    if(model.inputs.login.username === '' ||
+        model.inputs.login.password === '') {
+            model.app.wrongUserNamePasswordMessage = 'Username / password required.';
+            model.inputs.login.wrongUserNamePassword = true;
+            model.inputs.login.dropdown = true;
+            model.app.loggedInStatus= false;
+    }
+    else{
+        for(userKeys in model.data.users){
+            if(model.inputs.login.username === model.data.users[userKeys].username &&
+            model.inputs.login.password === model.data.users[userKeys].password){
+                model.inputs.login.dropdown = false;
+                model.app.loggedInStatus= true;
+                model.inputs.login.wrongUserNamePassword = false;
+                model.app.wrongUserNamePasswordMessage = '';
+                model.app.userId == userKeys
+                break;
+            }
+            else {
+                model.app.wrongUserNamePasswordMessage = 'Wrong username/ password.';
+                model.inputs.login.wrongUserNamePassword = true;
+                model.inputs.login.dropdown = true;
+                model.app.loggedInStatus= false;
+            }
+        }
+    }
+    updateView();
+}
+
+function removeCategory(item,cat){
+    model.data.items[item].category.splice(cat,1)
+    updateView()
+}
+
+function blowUpGalleryImg(img){
+    model.app.zoomedPic = img
+    updateView()
+}
+
+function unZoom(){
+    model.app.zoomedPic = false
+    updateView()
+}
+
+function addNewSubCategory(id){
+    if(model.inputs.product.adminAddSubCategory != ""){
+        model.data.items[id].category.push(model.inputs.product.adminAddSubCategory)
+        model.inputs.product.adminAddSubCategory = ""
+        updateView()   
+    }
+}
+
+function addToShoppingCart(productId){
+    for(let i = 0; i < model.data.users[model.app.userId].shoppingCart.length;i++){
+        if(model.data.users[model.app.userId].shoppingCart[i].id == productId){
+            model.data.users[model.app.userId].shoppingCart[i].quantity++
+            return
+        }
+    }
+    model.data.users[model.app.userId].shoppingCart.push({id:productId,quantity:1})
+    updateView()
+}
+
+function raiseBid(productId){
+    if(model.data.auctionListe[productId].bids[model.app.userId]){
+        model.data.auctionListe[productId].bids[model.app.userId].push(model.inputs.product.bidIncrease)
+    }
+    else{
+        model.data.auctionListe[productId].bids[model.app.userId] = [model.inputs.product.bidIncrease]
+    }
+    model.inputs.product.bidIncrease = 0
+    updateView()
+}
+
+function checkLogin()
+{
+    const users = model.data.users;
+
+    for (let userId in users)
+    {
+        const user = users[userId];
+        console.log(model.inputs.login.username)
+        console.log(model.inputs.login.password)
+
+        if(model.inputs.login.username === user.username && model.inputs.login.password === user.password)
+        {
+            model.app.loggedInStatus = user.permissions;
+            model.app.userId = userId;
+        }
+
+    }
+}
+
+function checkFilterBox(index){
+    model.inputs.category.categoryList[index].checked = !model.inputs.category.categoryList[index].checked
+    for(let i = 0; i<model.inputs.category.categoryList.length; i++){
+        if(model.inputs.category.categoryList[i].parent == index){
+            model.inputs.category.categoryList[i].checked = false
+        }
+    }
+    
+    updateView()
+}
+
+function changeView(view){
+    model.app.currentView = view
+    updateView()
+}
+
+function createProduct(){
+    const newProduct = {
+        title: model.inputs.createSale.title,
+        id: model.data.items.length +1,
+        description: model.inputs.createSale.description,
+        price:model.inputs.createSale.price,
+        minimumBid:model.inputs.createSale.minimumBid,
+        minimumBidAmmount:model.inputs.createSale.minimumBidAmmount,
+        auction:  model.inputs.createSale.auction,
+        deadline:model.inputs.createSale.deadline,
+        images: [],
+        deliver:model.inputs.createSale.deliver,
+        frontPage: model.inputs.createSale.frontPage,
+        category: model.inputs.createSale.categoryList,
+        mainImage: model.inputs.createSale.mainImage,
+        images : model.inputs.createSale.images,
+        inStock :true
+    }
+    model.data.items.push(newProduct);
+    saveMainCategory()
+}
+
+function saveMainCategory(){
+    let parentId;
+    let newParent = false
+    for(let i = 0; i<model.inputs.category.categoryList.length;i++){
+        if(model.inputs.category.categoryList[i].name == model.inputs.createSale.categoryList[0].name && model.inputs.category.categoryList[i].parent == -1){
+            parentId = i
+        }
+    }
+    if(!parentId){
+        newParent = true
+        parentId = model.inputs.category.categoryList.length
+        model.inputs.category.categoryList.push({
+            id:model.inputs.category.categoryList.length,
+            name:model.inputs.createSale.categoryList[0].name,
+            parent:-1,
+            checked:false
+        })
+    }
+    for(let i = 1; i<model.inputs.createSale.categoryList.length;i++){
+        if(newParent){
+            model.inputs.category.categoryList.push({
+                id: model.inputs.category.categoryList.length,
+                name: model.inputs.createSale.categoryList[i],
+                parent: parentId,
+                checked:false
+            })
+        }
+        else{
+            categoryExists = false
+            for(let j = 0; j<model.inputs.category.categoryList.length; j++){
+                if(model.inputs.category.categoryList[j].parent == parentId && model.inputs.category.categoryList[j].name ==  model.inputs.createSale.categoryList[i]){
+                    categoryExists = true
+                }
+            }
+            if(!categoryExists){
+                model.inputs.category.categoryList.push({
+                    id:model.inputs.category.categoryList.length,
+                    name:model.inputs.createSale.categoryList[i],
+                    parent:parentId,
+                    checked:false
+                })
+            }
+        }
+    }
+}
+
+function addMainCategory(){
+    if(model.inputs.createSale.mainCategory!=''){
+        model.inputs.createSale.categoryList[0] = model.inputs.createSale.mainCategory
+    }
+    model.inputs.createSale.mainCategory = ''
+    updateView()
+}
+
+function addSubCategory(){
+    if(!model.inputs.createSale.categoryList.includes(model.inputs.createSale.subCategory)){
+        model.inputs.createSale.categoryList.push(model.inputs.createSale.subCategory)
+    }
+    model.inputs.createSale.subCategory = ''
+    updateView()
+}
+
+function toogleLoginDrop(){
+    model.inputs.login.dropdown = !model.inputs.login.dropdown;
+    }
+
+function deleteCategory(index){
+    if(index == 0){
+        model.inputs.createSale.categoryList[0] = ""
+    }
+    else{
+        model.inputs.createSale.categoryList.splice(index,1)
+    }
+    updateView()
+}
+
+function insertImage(){
+    if(!model.inputs.createSale.images.includes(model.inputs.createSale.addImage)){
+        model.inputs.createSale.images.push(model.inputs.createSale.addImage)
+    }
+}
+
+function goToProduct(index){
+    model.app.currentView = "productPage"
+    model.app.currentProduct = index
+    updateView()
+}
+
+function filterItems(){
+    let filterArray = []
+    model.data.items.forEach(item =>{
+        if(item.title.includes(model.inputs.search.input) ||
+         item.description.includes(model.inputs.search.input) ||
+          item.category.includes(model.inputs.search.input)){
+            filterArray.push(eval(item.id))
+          }
+    })
+    console.log(filterArray)
+    filterArray = filterArray.filter(val => {
+        let included = true
+        for(let i = 0; i<model.inputs.category.categoryList.length; i++){
+            if(model.data.items[val-1].category.includes(model.inputs.category.categoryList[i].name)){
+                included = true
+                break
+            }
+            else{
+                included = false
+            }
+        }
+        return included? val : false
+    })
+    console.log(filterArray)
+    filterArray = filterArray.filter(val => {
+        if(model.data.items[val-1].price>=model.inputs.category.priceRange.min && model.data.items[val].price<=model.inputs.category.priceRange.max){
+            return val
+        }
+        else{
+            return false
+        }
+    })
+    return filterArray
+}
+
+function determinePriceLimits(){
+    let max = 0
+    for(let i = 0; i<model.data.items.length; i++){
+        if(model.data.items[i].price>max){
+            max = model.data.items[i].price
+        }
+    }
+    let min = max
+    for(let i = 0; i<model.data.items.length; i++){
+        if(model.data.items[i].price<min){
+            min = model.data.items[i].price
+        }
+    }
+    return {max:max,min:min}
+}
+
+function changePriceLevels(value){
+    model.inputs.category.priceRange.max = value
+    updateView()
+}
+
+let priceLimits = determinePriceLimits()
+model.inputs.category.priceRange.max = priceLimits.max
+model.inputs.category.priceRange.min = priceLimits.min
